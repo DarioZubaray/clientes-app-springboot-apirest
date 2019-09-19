7b2835ff724eb879123ad0c5441e8aa91c38b411
dario.zubaray <dzubaray@imagosur.com>
2019-09-19 09:47:47 -0300

    spring started project creado con spring-web, dev-tools, jpa y mysql como dependencias

---
+++ src/test/java/com/github/dariozubaray/springboot/api/rest/SpringbootApiRestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.dariozubaray.springboot.api.rest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootApiRestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
