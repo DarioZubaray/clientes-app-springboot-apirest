16e1af8e34a12120a6df4c1ed375f6d64da889f0
dario.zubaray <dzubaray@imagosur.com>
2019-09-23 09:07:52 -0300

    agregando CORS al controller con la anotacion CrossOrigin

--- src/main/java/com/github/dariozubaray/springboot/api/rest/models/controllers/ClienteRestController.java
+++ src/main/java/com/github/dariozubaray/springboot/api/rest/models/controllers/ClienteRestController.java
@@ -2,12 +2,14 @@ package com.github.dariozubaray.springboot.api.rest.models.controllers;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.github.dariozubaray.springboot.api.rest.models.entity.Cliente;
 import com.github.dariozubaray.springboot.api.rest.models.services.IClienteService;
 
+@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/api")
 public class ClienteRestController {
