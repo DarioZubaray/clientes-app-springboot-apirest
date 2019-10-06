40fa7bb4cbd5db22bc8d5c05753013dcbfe57bbb
dario zubaray <dzubaray@gmail.com>
2019-10-06 16:16:55 -0300

    configurando cors en spring security y filtro para OAuth2

--- src/main/java/com/github/dariozubaray/springboot/api/rest/auth/ResourceServerConfig.java
+++ src/main/java/com/github/dariozubaray/springboot/api/rest/auth/ResourceServerConfig.java
@@ -1,10 +1,19 @@
 package com.github.dariozubaray.springboot.api.rest.auth;
 
+import java.util.Arrays;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableResourceServer
@@ -19,7 +28,31 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 //            .antMatchers(HttpMethod.POST, "/api/clientes/upload").hasAnyRole("USER", "ADMIN")
 //            .antMatchers(HttpMethod.POST, "/api/clientes").hasRole("ADMIN")
 //            .antMatchers("/api/clientes/**").hasRole("ADMIN")
-            .anyRequest().authenticated();
+            .anyRequest().authenticated()
+            .and()
+            .cors().configurationSource(corsConfigurationSource());
         // @formatter: on
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(Arrays.asList("Content-type", "Authorization"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        CorsFilter corsFilter = new CorsFilter(corsConfigurationSource());
+        FilterRegistrationBean<CorsFilter> corsFilterBean = new FilterRegistrationBean<>(corsFilter);
+        corsFilterBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return corsFilterBean;
     }
 }
